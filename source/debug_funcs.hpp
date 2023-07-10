cd1ffdbbd5a28ce77cd164acc47e4c81c66dc8fb
b0rd2dEAth <ppkantorski@berkeley.edu>
2023-07-10 05:13:50 -0700

    Create debug_funcs.hpp

---
+++ source/debug_funcs.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <cstdio>
+#include <time.h>
+
+void logMessage(const std::string& message) {
+    std::time_t currentTime = std::time(nullptr);
+    std::string logEntry = std::asctime(std::localtime(&currentTime));
+    std::size_t lastNonNewline = logEntry.find_last_not_of("\r\n");
+    if (lastNonNewline != std::string::npos) {
+        logEntry.erase(lastNonNewline + 1);
+    }
+    logEntry = "[" + logEntry + "] " + message + "\n";
+
+    FILE* file = fopen("sdmc:/config/ultrahand/log.txt", "a");
+    if (file != nullptr) {
+        fputs(logEntry.c_str(), file);
+        fclose(file);
+    }
+}
