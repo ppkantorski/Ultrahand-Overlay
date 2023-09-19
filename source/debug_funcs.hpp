1eb5e06f542b622c874ea8c61f4eb6a9f341e3e3
b0rd2dEAth <ppkantorski@berkeley.edu>
2023-09-18 18:24:52 -0700

    Update debug_funcs.hpp

--- source/debug_funcs.hpp
+++ source/debug_funcs.hpp
@@ -1,3 +1,14 @@
+/********************************************************************************
+ * File: debug_funcs.hpp
+ * Author: ppkantorski
+ * Description:
+ *   This header file contains debugging functions for the Ultrahand-Overlay project.
+ *   These functions allow logging messages with timestamps to a log file.
+ *
+ *   For the latest updates and contributions, visit the project's GitHub repository.
+ *   (GitHub Repository: https://github.com/ppkantorski/Ultrahand-Overlay)
+ ********************************************************************************/
+
 #pragma once
 #include <cstdio>
 #include <time.h>
