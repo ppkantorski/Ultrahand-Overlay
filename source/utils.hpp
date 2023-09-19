2a491976ace3c1e5a63cceccb7fe96c6790207fc
b0rd2dEAth <ppkantorski@berkeley.edu>
2023-09-19 01:51:00 -0700

    Update utils.hpp

--- source/utils.hpp
+++ source/utils.hpp
@@ -3,7 +3,7 @@
  * Author: ppkantorski
  * Description:
  *   This header file contains utility functions and macros used in the
- *   Ultrahand-Overlay project. These functions and macros include definitions for
+ *   Ultrahand Overlay project. These functions and macros include definitions for
  *   various button keys, path variables, and command interpretation and execution.
  *
  *   For the latest updates and contributions, visit the project's GitHub repository.
