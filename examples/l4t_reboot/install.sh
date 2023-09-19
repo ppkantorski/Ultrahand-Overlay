35a185a63cc4de917d806a66856606640474874d
b0rd2dEAth <ppkantorski@berkeley.edu>
2023-09-18 22:42:54 -0700

    Update install.sh

--- examples/l4t_reboot/install.sh
+++ examples/l4t_reboot/install.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+################################################################################
+# File: install.sh
+# Author: ppkantorski
+# Description:
+#   This script is part of the Ultrahand-Overlay project and is responsible for
+#   installing and configuring the necessary components of the project. It
+#   automates the setup process for the Ultrahand-Overlay on your system.
+#
+#   Key Features:
+#   - Installation of required dependencies.
+#   - Configuration of system settings.
+#   - Setup of the Ultrahand-Overlay project.
+#
+#   Note: Please refer to the project documentation and README.md for detailed
+#   information on how to use and configure this script within the Ultrahand-Overlay.
+#
+#   For the latest updates and contributions, visit the project's GitHub repository.
+#   (GitHub Repository: https://github.com/ppkantorski/Ultrahand-Overlay)
+#
+# Copyright (c) 2023 ppkantorski
+# All rights reserved.
+################################################################################
 
 # Set the path to your Python script
 script_path="/usr/local/bin/l4t_reboot.py"
