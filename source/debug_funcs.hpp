ea72d9fd198deadd6afe374b9341ccaca88bf0c1
ppkantorski <ppkantorski@berkeley.edu>
2023-10-07 03:27:43 -0700

    Update debug_funcs.hpp

--- source/debug_funcs.hpp
+++ source/debug_funcs.hpp
@@ -14,14 +14,62 @@
 
 #pragma once
 #include <cstdio>
-#include <ctime>
-#include <iostream>
-#include <deque>
+#include <time.h>
 
+// Specify the log file path
 const std::string logFilePath = "sdmc:/config/ultrahand/log.txt";
-const int maxLines = 3000;
+const int maxLines = 5000;
 
-std::deque<std::string> logQueue;
+
+
+// This function looks at a .txt file and removes the begging lines until the log is of maxLines size.
+void trimLog(FILE* file) {
+    if (file != nullptr) {
+        // Read the existing lines into a buffer
+        fseek(file, 0, SEEK_END);
+        long fileSize = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        
+        char* buffer = nullptr;
+        if (fileSize > 0) {
+            buffer = new char[fileSize];
+            fread(buffer, 1, fileSize, file);
+        }
+        
+        // Close the file
+        fclose(file);
+        
+        // Reopen the file for writing
+        file = fopen(logFilePath.c_str(), "w");
+        if (file != nullptr) {
+            // Split the buffer into lines
+            char* line = strtok(buffer, "\n");
+            bool firstLine = true;
+            size_t lineCount = 0;
+            
+            while (line != nullptr) {
+                if (!firstLine) {
+                    fputc('\n', file);
+                }
+                
+                // Only append if the line count is less than maxLines
+                if (lineCount >= (fileSize / 2)) {
+                    fputs(line, file);
+                }
+                
+                line = strtok(nullptr, "\n");
+                firstLine = false;
+                ++lineCount;
+            }
+            
+            delete[] buffer;
+            
+            // Close the file
+            fclose(file);
+        }
+    }
+}
+
 
 /**
  * @brief Logs a message with a timestamp to a log file.
@@ -31,17 +79,18 @@ std::deque<std::string> logQueue;
 void logMessage(const std::string& message) {
     std::time_t currentTime = std::time(nullptr);
     std::string logEntry = std::asctime(std::localtime(&currentTime));
-    logEntry = "[" + logEntry.substr(0, logEntry.length() - 1) + "] " + message;
-    
-    logQueue.push_back(logEntry);
-    
-    if (logQueue.size() > maxLines) {
-        logQueue.pop_front();
+    std::size_t lastNonNewline = logEntry.find_last_not_of("\r\n");
+    if (lastNonNewline != std::string::npos) {
+        logEntry.erase(lastNonNewline + 1);
     }
-    
+    logEntry = "[" + logEntry + "] " + message + "\n";
+
     FILE* file = fopen(logFilePath.c_str(), "a");
     if (file != nullptr) {
-        fprintf(file, "%s\n", logEntry.c_str());
+        fputs(logEntry.c_str(), file);
+        //trimLog(file);
         fclose(file);
     }
 }
+
+
