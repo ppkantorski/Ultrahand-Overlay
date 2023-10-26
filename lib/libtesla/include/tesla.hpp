840e6ad138c2c7f6ced4324dbf3c2d9536b459a3
ppkantorski <ppkantorski@berkeley.edu>
2023-10-26 00:59:30 -0700

    Update tesla.hpp

--- lib/libtesla/include/tesla.hpp
+++ lib/libtesla/include/tesla.hpp
@@ -8,16 +8,13 @@
  *   of libtesla, an overlay executor. Within this file, you will find a collection of
  *   functions, menu structures, and interaction logic designed to facilitate the
  *   smooth execution and flexible customization of overlays within the project.
- * 
- *   Note: Refer to the project documentation and README.md for comprehensive
- *   usage and configuration information regarding the Ultrahand-Overlay.
- * 
+ *
  *   For the latest updates and contributions, visit the project's GitHub repository.
  *   (GitHub Repository: https://github.com/ppkantorski/Ultrahand-Overlay)
  *
  *   Note: Please be aware that this notice cannot be altered or removed. It is a part
  *   of the project's documentation and must remain intact.
- * 
+ *
  *  Copyright (c) 2023 ppkantorski
  *  All rights reserved.
  ********************************************************************************/
@@ -475,83 +472,7 @@ void localizeTimeStr(char* timeStr) {
 
 
 
-// Map of character widths
-static std::unordered_map<char, float> characterWidths = {
-    {'°', 0.25},
-    {'%', 0.98}, // not calibrated
-    {':', 0.25}, // not calibrated
-    {' ', 0.3},
-    {'+', 0.75},
-    {'-', 0.36},
-    {'_', 0.47},
-    {'&', 0.74},
-    {'(', 0.36},
-    {')', 0.36},
-    {'[', 0.3635},
-    {']', 0.3635},
-    {'A', 0.78},
-    {'B', 0.644},
-    {'C', 0.76},
-    {'D', 0.8},
-    {'E', 0.6},
-    {'F', 0.6},
-    {'G', 0.8},
-    {'H', 0.72},
-    {'I', 0.26},
-    {'J', 0.48},
-    {'K', 0.68},
-    {'L', 0.46},
-    {'M', 0.98},
-    {'N', 0.82},
-    {'O', 0.92},
-    {'P', 0.6},
-    {'Q', 0.9},
-    {'R', 0.6},
-    {'S', 0.56},
-    {'T', 0.64},
-    {'U', 0.80},
-    {'V', 0.76},
-    {'W', 1.14},
-    {'X', 0.66},
-    {'Y', 0.66},
-    {'Z', 0.74},
-    {'a', 0.6},
-    {'b', 0.66},
-    {'c', 0.56},
-    {'d', 0.66},
-    {'e', 0.6},
-    {'f', 0.28},
-    {'g', 0.6},
-    {'h', 0.6},
-    {'i', 0.25},
-    {'j', 0.36},
-    {'k', 0.56},
-    {'l', 0.28},
-    {'m', 0.94},
-    {'n', 0.582},
-    {'o', 0.656},
-    {'p', 0.66},
-    {'q', 0.68},
-    {'r', 0.36},
-    {'s', 0.5},
-    {'t', 0.37},
-    {'u', 0.6},
-    {'v', 0.50},
-    {'w', 0.87},
-    {'x', 0.54},
-    {'y', 0.53},
-    {'z', 0.5},
-    {'0', 0.66},
-    {'1', 0.66},
-    {'2', 0.66},
-    {'3', 0.66},
-    {'4', 0.66},
-    {'5', 0.66},
-    {'6', 0.66},
-    {'7', 0.66},
-    {'8', 0.66},
-    {'9', 0.66}
-};
+
 
 bool isValidHexColor(const std::string& hexColor) {
     // Check if the string is a valid hexadecimal color of the format "#RRGGBB"
@@ -2343,7 +2264,83 @@ namespace tsl {
 
         // CUSTOM SECTION START
         
-        
+        // Map of character widths
+        static std::unordered_map<char, float> characterWidths = {
+            {'°', 0.25},
+            {'%', 0.98}, // not calibrated
+            {':', 0.25}, // not calibrated
+            {' ', 0.3},
+            {'+', 0.75},
+            {'-', 0.36},
+            {'_', 0.47},
+            {'&', 0.74},
+            {'(', 0.36},
+            {')', 0.36},
+            {'[', 0.3635},
+            {']', 0.3635},
+            {'A', 0.78},
+            {'B', 0.644},
+            {'C', 0.76},
+            {'D', 0.8},
+            {'E', 0.6},
+            {'F', 0.6},
+            {'G', 0.8},
+            {'H', 0.72},
+            {'I', 0.26},
+            {'J', 0.48},
+            {'K', 0.68},
+            {'L', 0.46},
+            {'M', 0.98},
+            {'N', 0.82},
+            {'O', 0.92},
+            {'P', 0.6},
+            {'Q', 0.9},
+            {'R', 0.6},
+            {'S', 0.56},
+            {'T', 0.64},
+            {'U', 0.80},
+            {'V', 0.76},
+            {'W', 1.14},
+            {'X', 0.66},
+            {'Y', 0.66},
+            {'Z', 0.74},
+            {'a', 0.6},
+            {'b', 0.66},
+            {'c', 0.56},
+            {'d', 0.66},
+            {'e', 0.6},
+            {'f', 0.28},
+            {'g', 0.6},
+            {'h', 0.6},
+            {'i', 0.25},
+            {'j', 0.36},
+            {'k', 0.56},
+            {'l', 0.28},
+            {'m', 0.94},
+            {'n', 0.582},
+            {'o', 0.656},
+            {'p', 0.66},
+            {'q', 0.68},
+            {'r', 0.36},
+            {'s', 0.5},
+            {'t', 0.37},
+            {'u', 0.6},
+            {'v', 0.50},
+            {'w', 0.87},
+            {'x', 0.54},
+            {'y', 0.53},
+            {'z', 0.5},
+            {'0', 0.66},
+            {'1', 0.66},
+            {'2', 0.66},
+            {'3', 0.66},
+            {'4', 0.66},
+            {'5', 0.66},
+            {'6', 0.66},
+            {'7', 0.66},
+            {'8', 0.66},
+            {'9', 0.66}
+        };
         float calculateStringWidth(const std::string& str, int fontSize) {
             
             float totalWidth = 0;
@@ -4680,7 +4677,14 @@ namespace tsl {
         overlay->initScreen();
         overlay->changeTo(overlay->loadInitialGui());
 
-
+        // Argument parsing
+        //for (u8 arg = 0; arg < argc; arg++) {
+        //    if (strcasecmp(argv[arg], "--skipCombo") == 0) {
+        //        eventFire(&shData.comboEvent);
+        //        overlay->disableNextAnimation();
+        //    }
+        //}
+        
         // CUSTOM SECTION START
         // Argument parsing
         bool skipCombo = false;
@@ -4695,7 +4699,7 @@ namespace tsl {
         std::string settingsConfigPath = "sdmc:/config/ultrahand/config.ini";
         std::map<std::string, std::map<std::string, std::string>> settingsData = getParsedDataFromIniFile(settingsConfigPath);
         std::string inOverlayString;
-
+        
         if (settingsData.count("ultrahand") > 0 && settingsData["ultrahand"].count("in_overlay") > 0) {
             inOverlayString = settingsData["ultrahand"]["in_overlay"];
         } else {
@@ -4707,7 +4711,6 @@ namespace tsl {
             inOverlay = true;
             setIniFileValue(settingsConfigPath, "ultrahand", "in_overlay", "false");
         }
-        
         
         if (inOverlay && skipCombo) {
             eventFire(&shData.comboEvent);
