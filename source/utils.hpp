be5f5a1cfda31592d58c8761832e8c7aa0b65690
ppkantorski <ppkantorski@berkeley.edu>
2024-09-02 15:56:26 -0700

    Update utils.hpp

--- source/utils.hpp
+++ source/utils.hpp
@@ -163,6 +163,7 @@ static bool usingEmunand = true;
 void writeFuseIni(const std::string& outputPath, const char* data = nullptr) {
     std::ofstream outFile(outputPath);
     if (outFile) {
+        //outFile.write("; do not adjust these values manually unless they were not dumped correctly\n", 81);
         outFile.write("[", 1);
         outFile.write(FUSE_STR.c_str(), FUSE_STR.size());
         outFile.write("]\n", 2);
@@ -303,40 +304,40 @@ void formatVersion(uint64_t packed_version, int shift1, int shift2, int shift3, 
 
 const char* getMemoryType(uint64_t packed_version) {
     switch (packed_version) {
-        case 0: return "Samsung_K4F6E304HB-MGCH_4GB_LPDDR4_3200Mbps";
-        case 1: return "Hynix_H9HCNNNBPUMLHR-NLE_4GB_LPDDR4_3200Mbps";
-        case 2: return "Micron_MT53B512M32D2NP-062_WT:C_4GB_LPDDR4_3200Mbps";
-        case 3: return "Hynix_H9HCNNNBKMMLXR-NEE_4GB_LPDDR4X_4266Mbps";
-        case 4: return "Samsung_K4FHE3D4HM-MGCH_6GB_LPDDR4_3200Mbps";
-        case 5: return "Hynix_H9HCNNNBKMMLXR-NEE_4GB_LPDDR4X_4266Mbps";
-        case 6: return "Hynix_H9HCNNNBKMMLXR-NEE_4GB_LPDDR4X_4266Mbps";
-        case 7: return "Samsung_K4FBE3D4HM-MGXX_8GB_LPDDR4_3200Mbps";
-        case 8: return "Samsung_K4U6E3S4AM-MGCJ_4GB_LPDDR4X_3733Mbps";
-        case 9: return "Samsung_K4UBE3D4AM-MGCJ_8GB_LPDDR4X_3733Mbps";
-        case 10: return "Hynix_H9HCNNNBKMMLHR-NME_4GB_LPDDR4X_3733Mbps";
-        case 11: return "Micron_MT53E512M32D2NP-046_WT:E_4GB_LPDDR4X_4266Mbps";
-        case 12: return "Samsung_K4U6E3S4AM-MGCJ_4GB_LPDDR4X_3733Mbps";
-        case 13: return "Samsung_K4UBE3D4AM-MGCJ_8GB_LPDDR4X_3733Mbps";
-        case 14: return "Hynix_H9HCNNNBKMMLHR-NME_4GB_LPDDR4X_3733Mbps";
-        case 15: return "Micron_MT53E512M32D2NP-046_WT:E_4GB_LPDDR4X_4266Mbps";
-        case 17: return "Samsung_K4U6E3S4AA-MGCL_4GB_LPDDR4X_4266Mbps";
-        case 18: return "Samsung_K4UBE3D4AA-MGCL_8GB_LPDDR4X_4266Mbps";
-        case 19: return "Samsung_K4U6E3S4AA-MGCL_4GB_LPDDR4X_4266Mbps";
-        case 20: return "Samsung_K4U6E3S4AB-MGCL_4GB_LPDDR4X_4266Mbps";
-        case 21: return "Samsung_K4U6E3S4AB-MGCL_4GB_LPDDR4X_4266Mbps";
-        case 22: return "Samsung_K4U6E3S4AB-MGCL_4GB_LPDDR4X_4266Mbps";
-        case 23: return "Samsung_K4UBE3D4AA-MGCL_8GB_LPDDR4X_4266Mbps";
-        case 24: return "Samsung_K4U6E3S4AA-MGCL_4GB_LPDDR4X_4266Mbps";
-        case 25: return "Micron_MT53E512M32D2NP-046_WT:F_4GB_LPDDR4X_4266Mbps";
-        case 26: return "Micron_MT53E512M32D2NP-046_WT:F_4GB_LPDDR4X_4266Mbps";
-        case 27: return "Micron_MT53E512M32D2NP-046_WT:F_4GB_LPDDR4X_4266Mbps";
-        case 28: return "Samsung_K4UBE3D4AA-MGCL_8GB_LPDDR4X_4266Mbps";
-        case 29: return "Hynix_H54G46CYRBX267_4GB_LPDDR4X_4266Mbps";
-        case 30: return "Hynix_H54G46CYRBX267_4GB_LPDDR4X_4266Mbps";
-        case 31: return "Hynix_H54G46CYRBX267_4GB_LPDDR4X_4266Mbps";
-        case 32: return "Micron_MT53E512M32D1NP-046_WT:B_4GB_LPDDR4X_4266Mbps";
-        case 33: return "Micron_MT53E512M32D1NP-046_WT:B_4GB_LPDDR4X_4266Mbps";
-        case 34: return "Micron_MT53E512M32D1NP-046_WT:B_4GB_LPDDR4X_4266Mbps";
+        case 0: return "Samsung_K4F6E304HB-MGCH_4GB LPDDR4 3200Mbps";
+        case 1: return "Hynix_H9HCNNNBPUMLHR-NLE_4GB LPDDR4 3200Mbps";
+        case 2: return "Micron_MT53B512M32D2NP-062 WT:C_4GB LPDDR4 3200Mbps";
+        case 3: return "Hynix_H9HCNNNBKMMLXR-NEE_4GB LPDDR4X4266Mbps";
+        case 4: return "Samsung_K4FHE3D4HM-MGCH_6GB LPDDR4 3200Mbps";
+        case 5: return "Hynix_H9HCNNNBKMMLXR-NEE_4GB LPDDR4X4266Mbps";
+        case 6: return "Hynix_H9HCNNNBKMMLXR-NEE_4GB LPDDR4X4266Mbps";
+        case 7: return "Samsung_K4FBE3D4HM-MGXX_8GB LPDDR4 3200Mbps";
+        case 8: return "Samsung_K4U6E3S4AM-MGCJ_4GB LPDDR4X3733Mbps";
+        case 9: return "Samsung_K4UBE3D4AM-MGCJ_8GB LPDDR4X3733Mbps";
+        case 10: return "Hynix_H9HCNNNBKMMLHR-NME_4GB LPDDR4X3733Mbps";
+        case 11: return "Micron_MT53E512M32D2NP-046 WT:E_4GB LPDDR4X4266Mbps";
+        case 12: return "Samsung_K4U6E3S4AM-MGCJ_4GB LPDDR4X3733Mbps";
+        case 13: return "Samsung_K4UBE3D4AM-MGCJ_8GB LPDDR4X3733Mbps";
+        case 14: return "Hynix_H9HCNNNBKMMLHR-NME_4GB LPDDR4X3733Mbps";
+        case 15: return "Micron_MT53E512M32D2NP-046 WT:E_4GB LPDDR4X4266Mbps";
+        case 17: return "Samsung_K4U6E3S4AA-MGCL_4GB LPDDR4X4266Mbps";
+        case 18: return "Samsung_K4UBE3D4AA-MGCL_8GB LPDDR4X4266Mbps";
+        case 19: return "Samsung_K4U6E3S4AA-MGCL_4GB LPDDR4X4266Mbps";
+        case 20: return "Samsung_K4U6E3S4AB-MGCL_4GB LPDDR4X4266Mbps";
+        case 21: return "Samsung_K4U6E3S4AB-MGCL_4GB LPDDR4X4266Mbps";
+        case 22: return "Samsung_K4U6E3S4AB-MGCL_4GB LPDDR4X4266Mbps";
+        case 23: return "Samsung_K4UBE3D4AA-MGCL_8GB LPDDR4X4266Mbps";
+        case 24: return "Samsung_K4U6E3S4AA-MGCL_4GB LPDDR4X4266Mbps";
+        case 25: return "Micron_MT53E512M32D2NP-046 WT:F_4GB LPDDR4X4266Mbps";
+        case 26: return "Micron_MT53E512M32D2NP-046 WT:F_4GB LPDDR4X4266Mbps";
+        case 27: return "Micron_MT53E512M32D2NP-046 WT:F_4GB LPDDR4X4266Mbps";
+        case 28: return "Samsung_K4UBE3D4AA-MGCL_8GB LPDDR4X4266Mbps";
+        case 29: return "Hynix_H54G46CYRBX267_4GB LPDDR4X4266Mbps";
+        case 30: return "Hynix_H54G46CYRBX267_4GB LPDDR4X4266Mbps";
+        case 31: return "Hynix_H54G46CYRBX267_4GB LPDDR4X4266Mbps";
+        case 32: return "Micron_MT53E512M32D1NP-046 WT:B_4GB LPDDR4X4266Mbps";
+        case 33: return "Micron_MT53E512M32D1NP-046 WT:B_4GB LPDDR4X4266Mbps";
+        case 34: return "Micron_MT53E512M32D1NP-046 WT:B_4GB LPDDR4X4266Mbps";
         default: return "";
     }
 }
